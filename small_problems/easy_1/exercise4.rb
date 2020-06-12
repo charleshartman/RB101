d789ed9a268f07dcaa5918ebe505450b172d13c1
Charles A. Hartman <charles@hartman.dev>
2020-06-11 21:47:51 -0700

    another small problem

--- small_problems/easy_1/exercise4.rb
+++ small_problems/easy_1/exercise4.rb
@@ -8,8 +8,15 @@ vehicles = [
 ]
 
 def count_occurrences(input)
-  # input.each do |word|
-  #   
+  counter_hash = Hash.new
+  input.each do |word|
+    if counter_hash.key?(word)
+      counter_hash[word] += 1
+    else
+      counter_hash[word] = 1
+    end
+  end
+  counter_hash.each_pair { |key, value| puts "#{key} => #{value}" }
 end
 
 count_occurrences(vehicles)
@@ -23,3 +30,28 @@ count_occurrences(vehicles)
 # truck => 3
 # SUV => 1
 # motorcycle => 2
+
+# .plan
+# declare a new hash
+# loop through array
+# if element in array is not present in hash add it with key as name of element
+# and value (counter) of 1
+# else if element is already present in the array add another occurrence to the
+# value of that key (value += 1)
+# return value of new hash (p)
+
+=begin
+Book Solution
+def count_occurrences(array)
+  occurrences = {}
+
+  array.uniq.each do |element|
+    occurrences[element] = array.count(element)
+  end
+
+  occurrences.each do |element, count|
+    puts "#{element} => #{count}"
+  end
+end
+
+=end
