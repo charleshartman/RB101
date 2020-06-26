8e833685286d4fed7b3b7c85b0638ffc5639078a
Charles A. Hartman <charles@hartman.dev>
2020-06-25 21:44:00 -0700

    more lesson 5 practice problems

--- lesson_5/pp15.rb
+++ lesson_5/pp15.rb
@@ -3,6 +3,17 @@
 # Given this data structure write some code to return an array which
 # contains only the hashes where all the integers are even.
 
-arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+arr =
+  [{ a: [1, 2, 3] }, { b: [2, 4, 6], c: [3, 6], d: [4] }, { e: [8], f: [6, 10] }]
 
-p arr
+def only_even(array)
+  array.select! do |hash|
+    hash.all? do |_key, value|
+      value.all? do |num|
+        num.even?
+      end
+    end
+  end
+end
+
+p only_even(arr)
