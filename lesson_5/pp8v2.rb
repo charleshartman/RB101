7b514eed2e70d568107998f6b48b98bb035d9886
Charles A. Hartman <charles@hartman.dev>
2020-07-01 21:24:16 -0700

    lesson 5 again

--- lesson_5/pp8v2.rb
+++ lesson_5/pp8v2.rb
@@ -6,10 +6,12 @@
 hsh = { first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'],
         fourth: ['over', 'the', 'lazy', 'dog'] }
 
-vow = []
-hsh.each_value do |values|
+VOWELS = 'aeiou'
+
+hsh.each do |_, values|
   values.each do |value|
-    vow << value.scan(/[aeiou]/)
+    value.chars.each do |char|
+      puts char if VOWELS.include?(char)
+    end
   end
 end
-puts vow
