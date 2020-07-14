c09b946e4090dfa61c9a67e44b0d766f95ac7d36
Charles A. Hartman <charles@hartman.dev>
2020-07-13 17:28:32 -0700

    added scoreboard and match play, plus UI refinements

--- lesson_6/ttt_bonus.rb
+++ lesson_6/ttt_bonus.rb
@@ -1,7 +1,5 @@
 # ttt_bonus.rb
-# frozen_string_literal: true
-
-# Walk-through: Tic Tac Toe
+# Tic Tac Toe Bonus Features Assignment
 
 require 'pry'
 
@@ -13,19 +11,50 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-# scoreboard = {
-#   player: 0,
-#   computer: 0
-# }
+def clear_screen
+  system 'clear'
+  system 'cls'
+end
+
+scoreboard = {
+  Player: 0,
+  Computer: 0
+}
+
+def keep_score(new, scoreboard)
+  if new == 'Player'
+    scoreboard[:Player] += 1
+  elsif new == 'Computer'
+    scoreboard[:Computer] += 1
+  end
+end
+
+def reset_scoreboard(scoreboard)
+  scoreboard[:Player] = 0
+  scoreboard[:Computer] = 0
+end
+
+def show_scoreboard(scoreboard)
+  prompt("Match scoreboard | Player: #{scoreboard[:Player]}, " \
+                 "Computer: #{scoreboard[:Computer]}\n\n")
+end
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def show_header
+  clear_screen
+  puts ''
+  prompt("*** Welcome (back) to Tic Tac Toe! ***")
+  prompt("* Win five games to win the match. *")
+  puts ''
+  prompt("You are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}.")
+end
+
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
-  system 'clear'
-  puts "You are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  show_header
   puts ''
   puts '     |     |'
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -66,11 +95,11 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
+    prompt("Choose a square (#{joinor(empty_squares(brd))}):")
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
 
-    prompt "Sorry, that's not a valid choice."
+    prompt("Sorry, that's not a valid choice.")
   end
 
   brd[square] = PLAYER_MARKER
@@ -100,38 +129,54 @@ def detect_winner(brd)
   nil
 end
 
-def keep_score(new, totals)
-  if new == 'player'
-    totals[:player] += 1
-  elsif new == 'computer'
-    totals[:computer] += 1
-  end
-end
-
 loop do
-  board = initialize_board
+  # intro and current score here
+
+  reset_scoreboard(scoreboard)
 
   loop do
+    board = initialize_board
+
+    loop do
+      display_board(board)
+
+      show_scoreboard(scoreboard)
+
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
     display_board(board)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board)
+      prompt("#{detect_winner(board)} won!\n\n")
+      # iterate scoreboard here
+      keep_score(detect_winner(board), scoreboard)
+    else
+      prompt("It's a tie!\n\n")
+    end
+    # check score for match winner here
+    show_scoreboard(scoreboard)
+    sleep 2.25
+    if scoreboard[:Player] == 5
+      puts ''
+      prompt("*** You have won five rounds! You win the match! ***")
+      puts ''
+      break
+    elsif scoreboard[:Computer] == 5
+      puts ''
+      prompt("Computer has won five rounds! Computer wins the match!")
+      puts ''
+      break
+    end
   end
 
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  prompt 'Play again? (y or n)'
+  prompt('Do you want to play another match? (y or n)')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt 'Thank you for playing Tic Tac Toe! Goodbye.'
+prompt('Thank you for playing Tic Tac Toe! Goodbye.')
