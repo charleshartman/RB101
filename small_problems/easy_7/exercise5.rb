21cf88702818b755ef97c4ad7a54272b825e3f32
Charles A. Hartman <charles@hartman.dev>
2020-07-18 12:31:16 -0700

    more small problems

--- small_problems/easy_7/exercise5.rb
+++ small_problems/easy_7/exercise5.rb
@@ -21,7 +21,7 @@ p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
 p staggered_case('ALL_CAPS') == 'AlL_CaPs'
 p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
 
-=begin # book solution is nice 
+=begin # book solution is nice
 
 def staggered_case(string)
   result = ''
