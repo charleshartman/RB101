13a8dc7b5349d453e6c18e47d65835ab8a081e2a
Charles A. Hartman <charles@hartman.dev>
2020-07-21 09:35:19 -0700

    outline implementation

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -2,6 +2,8 @@
 
 SUITS = ['H', 'D', 'S', 'C']
 CARDS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+SYMBOLS = { "S" => "\u2660", "H" => "\u2665", "D" => "\u2666",
+            "C" => "\u2663" }
 
 def deck_builder(suits, cards)
   fresh_deck = []
@@ -16,3 +18,28 @@ end
 
 deck = deck_builder(SUITS, CARDS)
 p deck
+
+# assignment high level implementation (book)
+# 1. Initialize deck
+# 2. Deal cards to player and dealer
+# 3. Player turn: hit or stay
+#   - repeat until bust or "stay"
+# 4. If player bust, dealer wins.
+# 5. Dealer turn: hit or stay
+#   - repeat until total >= 17
+# 6. If dealer bust, player wins.
+# 7. Compare cards and declare winner.
+
+# my list of parts so far
+# -----------------------
+# def prompt
+# def clear
+# constant for suits (spelled out or unicode)
+# welcome/intro text
+# def deck_builder
+# def draw_card
+# def value_aces
+# def total_cards
+# def display_cards
+# def who_wins?
+# def display_winner
